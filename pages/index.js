13c11a732e2094a04339482c19d056fc25650b9c
MD ADIL ALAM <adil.alam@opalina.in>
2020-05-15 20:38:16 +0530

    my second commit

--- pages/index.js
+++ pages/index.js
@@ -18,37 +18,16 @@ const Home = () => {
 
   return (
     <div>
-      <Head>
+      {/* <Head>
         <title>Home</title>
         <link rel='icon' href='/favicon.ico' />
         <script src='/ejjs.js' type='text/javascript' />
-      </Head>
+      </Head> */}
 
-      <Nav />
+      {/* <Nav /> */}
 
       <div className='hero'>
-        <h1 className='title'>Welcome to Next.js!</h1>
-        <p className='description'>
-          To get started, edit <code>pages/index.js</code> and save to reload.
-        </p>
-
-        <div className='row'>
-          <a href='https://nextjs.org/docs' className='card'>
-            <h3>Documentation &rarr;</h3>
-            <p>Learn more about Next.js in the documentation.</p>
-          </a>
-          <a href='https://nextjs.org/learn' className='card'>
-            <h3>Next.js Learn &rarr;</h3>
-            <p>Learn about Next.js by following an interactive tutorial!</p>
-          </a>
-          <a
-            href='https://github.com/zeit/next.js/tree/master/examples'
-            className='card'
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub.</p>
-          </a>
-        </div>
+        <h1 className='title'>Test push notification</h1>
       </div>
 
       <style jsx>{`
